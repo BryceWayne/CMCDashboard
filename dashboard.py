d743287223e49dff8641b002a8f363d2ea0f97f8
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-12-10 18:37:24 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,21 @@ from bokeh.models.widgets import Slider, TextInput, Tabs, Panel, Button, DataTab
 from bokeh.models.widgets import NumberFormatter, TableColumn, Dropdown, RadioButtonGroup, Select
 from bokeh.plotting import figure
 from pprint import pprint
+import datetime
+from sklearn import preprocessing
+market = 'Bitcoin'
+z = datetime.datetime.today()
+z.strftime("%x")
+temp = str(z).split('-')
+current_day = temp[0]+temp[1]+temp[2].split(" ")[0]
+web = requests.get(f"https://coinmarketcap.com/currencies/{market.lower()}/historical-data/?start=20130428&end=" + current_day)
 
+dfs = pd.read_html(web.text)
+data = dfs[2]
+data = data.iloc[::-1]
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index(data['Date'], inplace=True)
+data = data[['Open*', 'High', 'Low', 'Close**', 'Volume', 'Market Cap']]
 
 """
 SETUP DATA
@@ -22,7 +36,7 @@ y1 = 1/(sigma*np.sqrt(2*pi))*np.exp(-0.5*((x1-mu)/sigma)**2)
 source1 = ColumnDataSource(data=dict(x=x1, y=y1))
 u1 = np.linspace(mu - 6 * sigma, mu + 6 * sigma, N)
 v1 = 1/(sigma*np.sqrt(2*pi))*np.exp(-0.5*((u1-mu)/sigma)**2)
-source1_reference = ColumnDataSource(data=dict(x=u1, y=v1))
+source1_reference = ColumnDataSource(dict(x=data.index, y=data['Close**']))
 
 x2 = np.random.randint(low=1, high=11, size=100)
 unique2, counts2 = np.unique(x2, return_counts=True)
