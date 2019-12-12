eea25f4f3a025b1c90e00654da60c482ce605a50
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-12-11 22:21:05 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,7 @@ def get_data(market='Bitcoin'):
 	LENGTH = 30
 	window1, window2 = LENGTH, 7*LENGTH
 	data[f'{window1} Day MA'] = data['Close**'].rolling(window=window1).mean()
-	data[f'{window2} Week MA'] = data['Close**'].rolling(window=window2).mean()
+	data[f'{window1} Week MA'] = data['Close**'].rolling(window=window2).mean()
 	data = data[['Open*', 'High', 'Low', 'Close**', 'Volume', 'Market Cap', 'Date']]
 	return data
 
@@ -56,7 +56,7 @@ price.vbar(df['Date'][inc], w, df['Open*'][inc], df['Close**'][inc], fill_color=
 price.vbar(df['Date'][dec], w, df['Open*'][dec], df['Close**'][dec], fill_color="#F2583E", line_color="black")
 
 MA = figure(plot_height=600, plot_width=int(PHI*600), title="Bitcoin", tools="crosshair,pan,reset,save,wheel_zoom", x_axis_type="datetime")
-MA.line(x='30 Day MA', y='210 Day MA', line_width=1, line_alpha=0.6, source=source)
+MA.line(x='30 Day MA', y='30 Week MA', line_width=1, line_alpha=0.6, source=source)
 """
 SETUP WIDGETS
 """
