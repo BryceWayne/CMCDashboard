0cdf6ac1e364a65b0e03c64ce12ae8233070d096
Batman <35088752+BryceWayne@users.noreply.github.com>
2019-12-29 12:58:46 -0800

    Update

--- dashboard.py
+++ dashboard.py
@@ -35,6 +35,8 @@ def get_data(market='Bitcoin'):
 	data = dfs[2]
 	data = data.iloc[::-1]
 	data['Date'] = pd.to_datetime(data['Date'])
+	data['Close'] = data['Close**']
+	data['Open'] = data['Open*']
 	LENGTH = 30
 	window1, window2 = LENGTH, 7*LENGTH
 	data['Daily MA'] = data['Close**'].rolling(window=window1).mean()
@@ -47,7 +49,33 @@ def get_data(market='Bitcoin'):
 	for _ in range(1, 10):
 		data[f"L{_}"] = _/10*np.ones_like(data['Close**'])
 	data['Scaled'] = data['Close**']/data['Close**'].max()
+	data = RSI(data)
 	return data
+
+def RSI(df, n=14):
+	prices = df['Close**']
+	deltas = np.diff(prices)
+	seed = deltas[:n+1]
+	up = seed[seed >= 0].sum()/n
+	down = -seed[seed < 0].sum()/n
+	rs = up/down
+	rsi = np.zeros_like(prices)
+	rsi[:n] = 100-100//(1 + rs)
+	for i in range(n, len(prices)):
+		delta = deltas[i-1]
+		if delta > 0:
+			upval = delta
+			downval = 0
+		else:
+			upval = 0
+			downval = -delta
+
+		up = (up*(n-1) + upval)/n
+		down = (down*(n-1) + downval)/n
+		rs = up/down
+		rsi[i] = 100-100/(1 + rs)
+	df['RSI'] = rsi
+	return df
 
 df = get_data()
 source = ColumnDataSource(df)
@@ -61,7 +89,7 @@ intro = Select(title="Cryptocurrency", value="Bitcoin",
 						'Tezos', 'XRP', 'EOS', 'Stellar', 'Cardano', '0x', 'Bitcoin-Cash'])
 
 ''' PRICE '''
-price = figure(plot_height=int(0.9*WINDOW), plot_width=int(PHI*WINDOW), title=intro.value, tools="xpan", x_axis_type="datetime",
+price = figure(plot_height=int(0.9*WINDOW), plot_width=int(PHI*WINDOW), title=intro.value, tools="xpan, hover", x_axis_type="datetime",
 				 y_axis_type="linear", x_range=(source.data['Date'][0], source.data['Date'][-1]),
 				 y_range=(min(source.data['Close**']), max(source.data['Close**'])))
 price.line(x='Date', y='Close**', line_width=1.618, source=source)
@@ -69,8 +97,24 @@ price.line(x='Date', y='Daily MA', line_width=1, line_alpha=0.8, line_color='red
 price.line(x='Date', y='Weekly MA', line_width=1, line_alpha=0.6, line_color='green', legend_label='Weekly MA', source=source)
 price.xaxis.major_label_orientation = np.pi/4
 price.yaxis.axis_label = 'Price'
-price.xaxis.axis_label = 'Date'
 price.grid.grid_line_alpha=0.3
+price_hover = price.select(dict(type=HoverTool))
+price_hover.tooltips = [
+		("Date", "@Date{%Y-%m-%d}"),
+        ("Open", "@Open{%0.0000f}"),
+        ("High", "@High{%0.0000f}"),
+        ("Low", "@Low{%0.0000f}"),
+        ("Close", "@Close{%0.0000f}"),
+        ("Volume", "@Volume{%0.00f}"),
+        ]
+price_hover.formatters = {
+        'Date': 'datetime',
+        'Open' : 'printf',
+        'High' : 'printf',
+        'Low' : 'printf',
+        'Close' : 'printf',
+        'Volume' : 'printf',
+    }
 
 select = figure(title="Drag the middle and edges of the selection box to change the range above",
                 plot_height=int(0.1*WINDOW), plot_width=int(PHI*WINDOW), y_range=price.y_range,
@@ -78,7 +122,7 @@ select = figure(title="Drag the middle and edges of the selection box to change 
                 tools="", toolbar_location=None, background_fill_color="#efefef")
 
 price_range = RangeTool(x_range=price.x_range)
-price_range.overlay.fill_color = "navy"
+price_range.overlay.fill_color = "pink"
 price_range.overlay.fill_alpha = 0.1618
 
 select.line('Date', 'Close**', source=source)
@@ -94,6 +138,30 @@ ma.line(x='Date', y="Daily MA", line_width=1, line_alpha=1, source=source, line_
 ma.line(x='Date', y="Weekly MA", line_width=1.618, line_alpha=0.6, source=source, line_color='green', legend_label='Weekly MA')
 ma_period = TextInput(value=str(30), title="Moving Average Period")
 
+''' RSI '''
+rsi_plot = figure(plot_height=int(0.9*WINDOW), plot_width=int(PHI*WINDOW), title=intro.value+' Relative Strength Index', tools="xpan", x_axis_type="datetime",
+				 y_axis_type="linear", x_range=(source.data['Date'][0], source.data['Date'][-1]),
+				 y_range=(min(source.data['RSI']), max(source.data['RSI'])))
+rsi_plot.line(x='Date', y='RSI', line_width=1.618, source=source)
+rsi_plot.xaxis.major_label_orientation = np.pi/4
+rsi_plot.yaxis.axis_label = 'RSI'
+rsi_plot.grid.grid_line_alpha=1/PHI
+
+select_rsi = figure(title="Drag the middle and edges of the selection box to change the range above",
+                plot_height=int(0.1*WINDOW), plot_width=int(PHI*WINDOW), y_range=rsi_plot.y_range,
+                x_axis_type="datetime", y_axis_type=None, x_range=(source.data['Date'][0], source.data['Date'][-1]),
+                tools="", toolbar_location=None, background_fill_color="#efefef")
+
+rsi_range = RangeTool(x_range=rsi_plot.x_range)
+rsi_range.overlay.fill_color = "pink"
+rsi_range.overlay.fill_alpha = 0.1618
+
+select_rsi.line('Date', 'RSI', source=source)
+select_rsi.ygrid.grid_line_color = None
+select_rsi.add_tools(rsi_range)
+select_rsi.toolbar.active_multi = rsi_range
+
+''' RISK '''
 risk = figure(plot_height=WINDOW, plot_width=int(PHI*WINDOW), title="Risk", tools="crosshair,pan,reset,save,wheel_zoom", x_axis_type="datetime")
 risk.xaxis.major_label_orientation = np.pi/4
 risk.grid.grid_line_alpha=0.3
@@ -127,30 +195,42 @@ def select_crypto(attr, old, new):
     price.x_range.end = select.x_range.end = source.data['Date'][-1]
     price.y_range.start = min(source.data['Close**'])
     price.y_range.end = max(source.data['Close**'])
+    rsi_plot.x_range.start = select_rsi.x_range.start = source.data['Date'][0]
+    rsi_plot.x_range.end = select_rsi.x_range.end = source.data['Date'][-1]
+    rsi_plot.y_range.start = min(source.data['RSI'])
+    rsi_plot.y_range.end = max(source.data['RSI'])
+    price_hover = price.select(dict(type=HoverTool))
 	
 intro.on_change('value', select_crypto)
 
-def update_y_range(attr, new, old):
-	x_range = (price_range.x_range.start, price_range.x_range.end)
-	dates = source.data['Date']
-	close = source.data['Close**']
-	price.y_range.start = min(close[dates.index(x_range[0]): dates.index(x_range[1])])
-	price.y_range.end = max(close[dates.index(x_range[0]): dates.index(x_range[1])])
+# def update_y_range(attr, new, old):
+# 	x_range = (price.x_range.start, price.x_range.end)
+# 	current_day = str(datetime.datetime.fromtimestamp((x_range[0])/1E3))
+# 	current_day = current_day.split(' ')[0] 
+# 	current_day = np.datetime64(current_day)
+# 	dates = source.data['Date']
+# 	print(current_day)
+# 	print(str(dates[0]).split('T')[0])
+# 	# first_day = datetime.datetime.strptime(str(dates[0]).strip('.')[0], '%Y-%m-%dT%H:%M:%S')
+# 	# print((first_day == current_day))
+# 	close = source.data['Close**']
+# 	# price.y_range.start = min(close)
+# 	# price.y_range.end = max(close)
 
-price_range.x_range.on_change('start', update_y_range)
-price_range.x_range.on_change('end', update_y_range)
+# price_range.x_range.on_change('start', update_y_range)
+# price_range.x_range.on_change('end', update_y_range)
 
-def change_ma_period(attr, old, new):
-	df = pd.DataFrame(source.data)
-	df['Daily MA'] = df['Close**'].rolling(window=int(ma_period.value)).mean()
-	df['Weekly MA'] = df['Close**'].rolling(window=7*int(ma_period.value)).mean()
-	df['Risk'] = df['Daily MA']/df['Weekly MA']
-	min_max_scaler = preprocessing.MinMaxScaler()
-	np_scaled = min_max_scaler.fit_transform(df[['Risk']])
-	df['Risk'] = np_scaled
-	source.data = df.to_dict('list')
+# def change_ma_period(attr, old, new):
+# 	df = pd.DataFrame(source.data)
+# 	df['Daily MA'] = df['Close**'].rolling(window=int(ma_period.value)).mean()
+# 	df['Weekly MA'] = df['Close**'].rolling(window=7*int(ma_period.value)).mean()
+# 	df['Risk'] = df['Daily MA']/df['Weekly MA']
+# 	min_max_scaler = preprocessing.MinMaxScaler()
+# 	np_scaled = min_max_scaler.fit_transform(df[['Risk']])
+# 	df['Risk'] = np_scaled
+# 	source.data = df.to_dict('list')
 
-ma_period.on_change('value', change_ma_period)
+# ma_period.on_change('value', change_ma_period)
 
 # Set up layouts and add to document
 
@@ -160,8 +240,11 @@ tab0 = Panel(child=tab0, title="Crypto Selection")
 tab1 = row(column(price, select), width=int(PHI*400))
 tab1 = Panel(child=tab1, title="Price")
 
-tab2 = row(ma, column(ma_period), width=int(PHI*400))
-tab2 = Panel(child=tab2, title="Moving Averages")
+# tab2 = row(ma, column(ma_period), width=int(PHI*400))
+# tab2 = Panel(child=tab2, title="Moving Averages")
+tab2 = row(column(rsi_plot, select_rsi), width=int(PHI*400))
+tab2 = Panel(child=tab2, title="RSI")
+
 tab3 = row(risk, width=int(PHI*400))
 
 tab3 = Panel(child=tab3, title="Risk")
